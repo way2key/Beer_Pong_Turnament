cdaac0f1665786afb8e7f467af1bb23c7f265c25
way2key <olivier_dancona@hotmail.com>
2016-08-14 21:09:41 +0200

    adaptation of functions.php

---
+++ functions.php
@@ -0,0 +1,63 @@
+<?php
+function amax($array){
+    if(is_array($array)){
+
+    	$best=null;
+        foreach($array as $key => $value){
+            //echo $key;
+            //echo $value;
+            if($value==max($array)){
+            	echo $key;
+            }
+        }
+        return $key;
+
+
+
+    }else{
+        return $array;
+    }
+}
+
+function notempty($value,$needle){
+if(!empty($value)){
+    echo (in_array($needle, $value))? 'checked':'';
+    }
+}
+function notempty2($value,$needle){
+if(!empty($value)){
+    echo (in_array($needle, $value))? 'checked':'';
+    }
+}
+function notempty3($value,$needle){
+if(!empty($value)){
+    echo (in_array($needle, $value))? 'checked':'';
+    }
+}
+function notempty4($value,$needle){
+if(!empty($value)){
+    echo (in_array($needle, $value))? 'checked':'';
+    }
+}
+function wichwin($player1,$player2,$set){
+if(in_array($player1, $set)){
+    return $player1;
+}elseif(in_array($player2,$set)){
+    return $player2;
+}else{
+    return false;
+}
+}
+function wichlose($player1,$player2,$set){
+if(in_array($player1, $set)){
+    return $player2;
+}elseif(in_array($player2,$set)){
+    return $player1;
+}else{
+    return false;
+}
+}
+function winat3($player1,$player2,$player3,$set){
+    return True;
+}
+?>
